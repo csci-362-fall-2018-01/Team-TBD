f527f115bd7e19031e8a14a6359db4d8bd244f17
Sarah Nicholson <sarahryann11@outlook.com>
2018-10-22 12:31:45 -0400

    adding basic script and output file

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -1,3 +1,48 @@
 #!/bin/bash
-# myList - Team TBD
+# Team TBD
 
+# check if temp directory is empty. If not, remove files.
+DIR="temp/"
+if [ "$(ls -A $DIR)" ]; 
+then
+	echo "Removing all files from temp"
+	rm temp/*
+else
+	echo "No files to remove"
+	
+# load basic html
+while read LINE; do
+	echo "$LINE" >> temp/output.html
+done
+
+# loops through all test cases in the directory.
+for filename in testCases/*.txt; do
+	i="${filename//[^0-9]/}"
+	echo "Performing test $i."
+	# create an array and populate it with contents of testCase file
+	ARRAY=()
+	while read LINE; do
+		ARRAY+=("$LINE")
+	done < "$filename"
+
+# creating table
+	echo "<tr>
+	<td>${ARRAY[0]}</td>
+	<td><a name='"${ARRAY[1]}"'></a></td>
+	<td>${ARRAY[2]}</td>
+	<td>${ARRAY[3]}</td>
+	<td>${ARRAY[4]}</td>" >> temp/output.html
+	fi
+
+	# running actual test and getting the output
+	cd testCasesExecutables/
+	OUTPUT=$((insert testFile here) ${ARRAY[2]} ${ARRAY[3]})
+	echo "<td>$OUTPUT</td>" >> ../temp/output.html
+	fi
+done
+	
+# closing table
+echo "</table>" >> temp/output.html
+
+# open in browser
+xdg-open index.html
