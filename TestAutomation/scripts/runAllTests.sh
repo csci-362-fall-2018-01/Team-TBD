0626f8f436a103361f23f871ebd67cd6542f8a6c
sarahryann11 <sarahryann11@outlook.com>
2018-11-07 16:19:13 -0500

    updating script

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -16,7 +16,7 @@ fi
 # load basic html
 #while read LINE; do
 #	echo "$LINE" >> temp/output.html
-#done < "style/style.css"
+#done #< "style/style.css"
 
 # create the html file
 echo "<html>
@@ -29,35 +29,46 @@ echo "<html>
 
 echo "<body>
     <script src='../project/src/index.js'></script>" >> temp/output.html
+echo "<table align='center' border='1px'>
+<tr>
+<th>Test</th>
+<th>Requirement</th>
+<th>Component</th>
+<th>Method</th>
+<th>Input</th>
+<th>Oracle</th>
+<th>Result</th>
+</tr>" >> temp/output.html
 
 # loops through all test cases in the directory.
 for filename in testCases/*.txt; do
 	i="${filename//[^0-9]/}"
 	echo "Performing test $i."
-	# create an array and populate it with contents of testCase file
+	# create array and populate with contents of testCase files
 	ARRAY=()
 	while read LINE; do
 		ARRAY+=("$LINE")
 	done < "$filename"
 
 # creating table
-	echo "<table border='1px'><tr>
-	<td>${ARRAY[0]}</td>
+	echo "
+	<tr><td>${ARRAY[0]}</td>
 	<td>${ARRAY[1]}</td>
 	<td>${ARRAY[2]}</td>
 	<td>${ARRAY[3]}</td>
 	<td>${ARRAY[4]}</td>
 	<td>${ARRAY[5]}</td></tr>" >> temp/output.html
 
-	# running actual test and getting the output
-	#cd project/src
-	OUTPUT=$((../project/src/index.js) ${ARRAY[2]} ${ARRAY[3]})
-	echo "<td>$OUTPUT</td>" >> temp/output.html
+	# running actual test and getting output
+	#cd project/src/
+	#OUTPUT=$(index.js ${ARRAY[2]} ${ARRAY[3]})
+	#echo "<td>$OUTPUT</td></tr>" >> ../../temp/output.html
 	
-# closing table
-echo "</table>" >> temp/output.html
 done
 
+# closing table
+echo "</table>" >> temp/output.html
+echo "<br><br>" >> temp/output.html
 echo "</body>" >> temp/output.html
 echo "</html>" >> temp/output.html
 
