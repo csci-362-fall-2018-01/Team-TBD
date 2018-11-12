7e179c3153d3542b762b548f168246cc80c75bb8
sarahryann11 <sarahryann11@outlook.com>
2018-11-11 23:06:22 -0500

    updating tests

--- TestAutomation/project/src/index.js
+++ TestAutomation/project/src/index.js
@@ -38,17 +38,22 @@ function workspaceTest_tearDown() {
   workspace.dispose();
 }
 
+/**************************TEST CASE 1******************************/
+
 function test_safeName () {
 	var varDB = new Blockly.Names('window,door');
 	assertEquals('Is Safe Name', 'fooBar', varDB.safeName_('fooBar'));
 	//assertEquals('SafeName special chars.', '_______', varDB.safeName_('%$@)<.*'));
-	//assertEquals('SafeName empty.', 'unnamed', varDB.safeName_(''));
 }
 
-function test_safeName () {
-	var varDB = new Blockly.Names('window,door');
-	assertEquals('SafeName empty.', 'unnamed', varDB.safeName_(''));
+/**************************TEST CASE 2******************************/
+
+function test_commonWordSuffix() {
+  var len = Blockly.utils.commonWordSuffix('Xabc de,Yabc de'.split(','));
+  assertEquals('One word', 3, len);
 }
+
+/**************************TEST CASE 3******************************/
 
 function test_getName() {
   var varDB = new Blockly.Names('window,door');
@@ -56,27 +61,54 @@ function test_getName() {
   assertEquals('Name get #1.', 'Foo_bar', varDB.getName('Foo.bar', 'var'));
 }
 
+/**************************TEST CASE 4******************************/
+
 function test_getDistinctName() {
   var varDB = new Blockly.Names('window,door');
   assertEquals('Name distinct #1.', 'Foo_bar',
                varDB.getDistinctName('Foo.bar', 'var'));
 }
 
+/**************************TEST CASE 5******************************/
+
 function test_nameEquals() {
   assertTrue('Names equal.', Blockly.Names.equals('Foo.bar', 'Foo.bar'));
 }
 
-function test_nameEquals() {
-  assertFalse('Names not equal.', Blockly.Names.equals('Foo.bar', 'Foo_bar'));
-}
+/**************************TEST CASE 6******************************/
 
 function test_commonWordPrefix() {
 	var len = Blockly.utils.commonWordPrefix('abc de,abc de Y'.split(','));
   	assertEquals('Overflow yes', 6, len);
-	//var len = Blockly.utils.commonWordPrefix('aa,abc,de,gd,ax'.split(','));
-	//assertEquals('No prefix', 0, len);
-	//len = Blockly.utils.commonWordPrefix('one,two,three,four,five'.split(','));
-  	//assertEquals('No prefix', 0, len);
+}
+
+/**************************TEST CASE 7******************************/
+
+function test_shortestStringLength() {
+  var len = Blockly.utils.shortestStringLength([]);
+  assertEquals('Empty list', 0, len);
+}
+
+/**************************TEST CASE 8******************************/
+
+function test_startsWith() {
+  assertEquals('Does not start with', false, Blockly.utils.startsWith('123', '2'));
+}
+
+/**************************TEST CASE 9******************************/
+
+function test_arrayRemove() {
+  var arr = [1, 2, 3, 2];
+  assertEquals('Remove item', true, Blockly.utils.arrayRemove(arr, 2));
+}
+
+/**************************TEST CASE 10******************************/
+
+function test_getVariable_NotFound() {
+  variableMapTest_setUp();
+  var result = variable_map.getVariable('name1');
+  assertNull(result);
+  variableMapTest_tearDown();
 }
 
 /*function test_getVariableTypes_Trivial () {
