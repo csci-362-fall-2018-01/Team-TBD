df9f3c406592c0a70c3188cb0da02125a31131d7
sarahryann11 <sarahryann11@outlook.com>
2018-11-26 11:34:46 -0500

    adding faults

--- TestAutomation/testCaseExecutables/testCase04.js
+++ TestAutomation/testCaseExecutables/testCase04.js
@@ -1,8 +1,18 @@
 
-/**************************TEST CASE 4******************************/
+/**************************TEST CASE 4*****************************
 
 function test_getDistinctName() {
   var varDB = new Blockly.Names('window,door');
   assertEquals('Name distinct #1.', 'Foo_bar',
                varDB.getDistinctName('Foo.bar', 'var'));
 }
+*/
+
+/**************************FAULT INJECTION 4******************************/
+
+function test_getDistinctName() {
+  var varDB = new Blockly.Names('window,door');
+  assertNotEquals('Name distinct #1.', 'Foo_bar',
+               varDB.getDistinctName('Foo.bar', 'var'));
+}
+
